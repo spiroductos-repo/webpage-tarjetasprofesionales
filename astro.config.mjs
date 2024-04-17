2cf45c7d3fa47e7154ffe4967f1e9c2d7faa255a
michaelgy <michael801898@gmail.com>
2024-04-16 22:59:33 -0500

    astro deploy to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 import icon from "astro-icon";
-import { defineConfig } from 'astro/config';
-
-import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon(), compress()],
-  output: "server",
-  adapter: vercel()
+  output: "static",
+  site: "https://spiroductos-repo.github.io",
+  base: "webpage-tarjetasprofesionales",
 });
